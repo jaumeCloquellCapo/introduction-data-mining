e546d5643c02008761f5575ddff33b3e1468e3f2
jaume cloquell <jaumecloquell@hotmail.com>
2019-03-14 17:33:14 +0100

    .

---
+++ Laboratorio regresion/Untitled.R
@@ -0,0 +1,32 @@
+require(MASS)
+require(ISLR)
+
+temp <- Boston
+plotY <- function (x,y) {
+  plot(temp[,y]~temp[,x], xlab=paste(names(temp)[x]," X",x,sep=""),
+       ylab=names(temp)[y])
+}
+par(mfrow=c(3,4))
+x <- sapply(1:(dim(temp)[2]-1), plotY, dim(temp)[2])
+par(mfrow=c(1,1))
+
+par(mfrow=c(3,3))
+x <- sapply(c(1, 5, 6, 7, 8, 10, 11, 12, 13), plotY, dim(temp)[2])
+par(mfrow=c(1,1))
+
+fit1=lm(medv~lstat,data=Boston)
+fit1
+
+fit2=lm(medv~rm,data=Boston)
+fit2
+
+summary(fit1)
+par(mfrow=c(2,1))
+plot(medv~lstat,Boston)
+abline(fit1,col="red")
+confint(fit1)
+
+
+fitprueba=lm(medv~lstat +rm +I(lstat * rm) +I(lstat^2) +I(lstat^2 * rm),Boston) summary(fitprueba)
+plot(medv~lstat)
+points(lstat,fitted(fitprueba),col="red",pch=20)
